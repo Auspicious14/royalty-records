13f870c37694c0beb4925b58f9ce83b5b091412d
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-09 19:52:39 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 
 import type { Metadata } from 'next'
 import './globals.css'
-import { AppContextProvider } from '@/context/AppContext'
+import { AppContextProvider } from '@/context'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
