c7fb9f34fca71ede5bf736335faab61a98c7caa6
Auspicious <uthmanabdulganiyu2019@gmail.com>
2025-08-10 07:33:27 +0100

    Update page.tsx

--- src/modules/home/page.tsx
+++ src/modules/home/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import ArtistsSection from '@/modules/artist/components/ArtistGrid'
 import MusicSection from '@/modules/music/components/MusicGrid'
-import VideosSection from '@/modules/videos/components/VideoGrid'
+import VideosSection from '@/modules/video/components/VideoGrid'
 import ContactSection from '@/modules/contact/components/ContactInfo'
 
 export default function HomePage() {
